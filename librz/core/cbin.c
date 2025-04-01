f9c7f3b7b4d5baba4b181df327411dedcdaf1871
ahmed -k <ahmedkamal200427@gmail.com>
2025-04-01 16:33:14 +0200

    Add izx command for data and code xrefs

--- librz/core/cbin.c
+++ librz/core/cbin.c
@@ -2946,6 +2946,36 @@ RZ_API bool rz_core_bin_whole_strings_print(RZ_NONNULL RzCore *core, RZ_NONNULL 
 	return res;
 }
 
+RZ_IPI bool rz_core_bin_xrefs_strings_print(RZ_NONNULL RzCore *core, RZ_NONNULL RzBinFile *bf, RZ_NONNULL RzCmdStateOutput *state) {
+	rz_return_val_if_fail(core && state, false);
+
+	RzPVector *l = rz_core_bin_whole_strings(core, bf);
+	RzPVector *ll = rz_pvector_new(NULL);
+	
+	void ** iter;
+	rz_pvector_foreach (l, iter) {
+		RzBinString* string = *iter;
+		RzList *list = rz_analysis_xrefs_get_to(core->analysis, string->paddr);
+		RzAnalysisXRef *xref;
+		RzListIter *iterator;
+		rz_list_foreach (list, iterator, xref) {
+			switch (xref->type) {
+				case RZ_ANALYSIS_XREF_TYPE_CODE:
+				case RZ_ANALYSIS_XREF_TYPE_DATA:
+					if(!rz_pvector_contains(ll,string)){
+						rz_pvector_push_front(ll,string);
+					}
+				default:
+					break;
+			}
+		}
+	}
+	bool res = strings_print(core, state, ll);
+	rz_pvector_free(l);
+	rz_pvector_free(ll);
+	return res;
+}
+
 static const char *get_filename(RzBinInfo *info, RzIODesc *desc) {
 	if (info && info->file) {
 		return info->file;
