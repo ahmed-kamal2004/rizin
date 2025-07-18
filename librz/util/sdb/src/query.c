1b2dd45160f6c0b955813ce22f2acf8520a2d4ca
ahmed -k <ahmedkamal200427@gmail.com>
2025-07-18 03:19:09 +0300

    Remove sdb_fmt and replace with rz_strf

--- librz/util/sdb/src/query.c
+++ librz/util/sdb/src/query.c
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <ctype.h>
 #include "sdb.h"
+#include <rz_util.h>
 
 typedef struct {
 	char *buf;
@@ -214,7 +215,9 @@ repeat:
 		if (next) {
 			*next = 0;
 		}
-		out_concat(sdb_fmt("0x%08x\n", sdb_hash(p)));
+		char k[256];
+		char *ret = rz_strf(k, "0x%08x\n", sdb_hash(p));
+		out_concat(ret);
 		if (next) {
 			*next = ';';
 		}
